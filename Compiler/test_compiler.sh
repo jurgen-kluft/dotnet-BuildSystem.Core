84ea7f6fe029bf38922ab47fec2365bdf6147a61
Jurgen <jurgen.kluft@gmail.com>
2022-09-24 19:47:15 +0800

    * build.props; do not set document output folder * .gitignore; add .sh and .bat

---
+++ Compiler/test_compiler.sh
@@ -0,0 +1,1 @@
+dotnet run ./bin/Compiler.dll -- -name \"MJ\" -platform \"MAC\" -target \"MAC\" -territory \"Europe\" -basepath \"/Users/obnosis1\" -srcpath \"/Users/obnosis1/Data/Assets\" -gddpath \"/Users/obnosis1/Data/GameData\" -pubpath \"/Users/obnosis1/Data/Publish.%PLATFORM%\" -dstpath \"/Users/obnosis1/Data/Bin.%PLATFORM%\" -toolpath \"/Users/obnosis1/Data/Tools\"
